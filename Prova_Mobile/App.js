0f4b7e5e7e0daa1a47182f7465101e8682acae84
Emanuellacgomes <168464880+Emanuellacgomes@users.noreply.github.com>
2025-04-09 14:25:24 -0300

    main

---
+++ Prova_Mobile/App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import 'react-native-gesture-handler';
+import Login from './screens/Login';
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Login/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
---
+++ Prova_Mobile/screens/Login.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'; 
+export default function Login(){
+    return(
+        <View style={styles.container}>
+            <Text>Oi</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#D6FEEE',
+    },
+  });
